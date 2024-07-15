c5c1ed085f707fa5aa8ce3e9e7544daeb9838c0f
rony.richard11 <rony.richard11@gmail.com>
2024-07-15 12:28:26 -0600

    vercel update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,30 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var socket = io();
+    var socket = io(window.location.origin); // Use absolute URL for WebSocket connection
     var quill = new Quill('#editor', {
         theme: 'snow',
         modules: {
             toolbar: '#toolbar'
         }
     });
-  
+
     var urlParams = new URLSearchParams(window.location.search);
     var sessionId = urlParams.get('session');
     document.getElementById('session-id').textContent = sessionId; // Set the session ID in the HTML
-  
+
     socket.emit('join-session', sessionId);
-  
+
     socket.on('receive-change', function(delta) {
         quill.updateContents(delta, 'silent');
     });
-  
+
     socket.on('load-document', function(documentState) {
         quill.setContents(documentState, 'silent'); // Set the contents without triggering the 'text-change' event
     });
-  
+
     quill.on('text-change', function(delta, oldDelta, source) {
         if (source === 'user') {
             socket.emit('text-change', { sessionId: sessionId, change: delta });
         }
     });
-  });
-  
+});
